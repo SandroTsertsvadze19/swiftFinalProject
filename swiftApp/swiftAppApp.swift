a2b31d3f2e50067491bdad307c9a4d8043dc9112
Sandro Tsertsvadze <76789502+Sandrex233@users.noreply.github.com>
2025-01-19 12:14:34 +0000

    Initial Commit

---
+++ swiftApp/swiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftAppApp.swift
+//  swiftApp
+//
+//  Created by builder on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
